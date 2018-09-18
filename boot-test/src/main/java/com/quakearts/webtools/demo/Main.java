e50b9c19f412264a2ddb54b0790eaa2c7f71eb6f
Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>
2018-09-18 10:52:27 +0000

    Final version of demo

--- boot-test/src/main/java/com/quakearts/webtools/demo/Main.java
+++ boot-test/src/main/java/com/quakearts/webtools/demo/Main.java
@@ -1,11 +1,6 @@
 package com.quakearts.webtools.demo;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.text.MessageFormat;
-import javax.net.ServerSocketFactory;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,41 +27,7 @@ public class Main
 		ContextDependencySpiFactory.getInstance().getContextDependencySpi().initiateContextDependency();
 		EmbeddedWebServerSpiFactory.getInstance().getEmbeddedWebServerSpi().initiateEmbeddedWebServer();
 		
-		LOGGER.info(MessageFormat.format("Started in {0, time, ss.S} seconds", System.currentTimeMillis()-start));
-
-		int port = Integer.parseInt(System.getProperty("shutdown.port", "9999"));
-		LOGGER.info("Listening for shutdown command on port "+port);
-
-		ServerSocket shutdownSocket;
-		
-		try {
-			shutdownSocket = ServerSocketFactory.getDefault().createServerSocket(port);
-		} catch (IOException e) {
-			LOGGER.error("Exception of type " + e.getClass().getName() + " was thrown. Message is " + e.getMessage()
-					+ ". Exception occured whiles getting shutdown hook");
-			return;
-		}
-		
-		while(true){
-			Socket commandSocket;
-			try {
-				commandSocket = shutdownSocket.accept();
-				if(commandSocket.getInetAddress().isLoopbackAddress()){
-					int b = commandSocket.getInputStream().read();
-					if(b==0xFA) {
-						LOGGER.info("Shutdown called....");
-						
-						JavaNamingDirectorySpiFactory.getInstance().getJavaNamingDirectorySpi().shutdownJNDIService();;
-						ContextDependencySpiFactory.getInstance().getContextDependencySpi().shutDownContextDependency();
-						EmbeddedWebServerSpiFactory.getInstance().getEmbeddedWebServerSpi().shutdownEmbeddedWebServer();
-
-						LOGGER.info("Shutdown complete");
-						break;
-					}
-				}
-			} catch (IOException e) {
-				LOGGER.error("Could not understand message from shutdown command:"+e.getMessage());
-			}
-		}
+		String startup = MessageFormat.format("Started in {0, time, ss.S} seconds", System.currentTimeMillis()-start);
+		LOGGER.info(startup);
     }
 }
